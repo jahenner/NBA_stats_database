62e4732564127724194bac44673b976521ab1ca4
Alex Henner <jahenner@gmail.com>
2022-06-05 16:20:55 +0000

    player stat page all hooked up

--- db-controller.js
+++ db-controller.js
@@ -66,7 +66,7 @@ app.get('/GetPlayers', function(req,res) {
 })
 
 app.get('/GetStats', function(req,res) {
-    const query1 = "SELECT CONCAT(Players.first_name, ' ', Players.last_name) AS player_name, Games.date, IF(Players.current_team=Teams_has_Games.home_team_id, Teams_has_Games.home_team_id, Teams_has_Games.away_team_id) as opposing_team, Players_has_Games.rebounds, Players_has_Games.blocks, Players_has_Games.steals, Players_has_Games.turnovers, Players_has_Games.minutes_played, Players_has_Games.started_game, Players_has_Games.freethrows_attempt, Players_has_Games.freethrows_made, Players_has_Games.field_goals_attempt, Players_has_Games.field_goals_made, Players_has_Games.3_points_attempt, Players_has_Games.3_points_made, Players_has_Games.assists, Players_has_Games.fouls, Players_has_Games.player_id, Players_has_Games.game_id FROM Players_has_Games JOIN Players ON Players_has_Games.player_id = Players.player_id JOIN Games ON Games.game_id=Players_has_Games.game_id JOIN Teams_has_Games ON Teams_has_Games.game_id=Games.game_id;"
+    const query1 = "SELECT CONCAT(Players.first_name, ' ', Players.last_name) AS player_name, Games.date, IF(Players.current_team=Teams_has_Games.home_team_id, Teams_has_Games.home_team_id, Teams_has_Games.away_team_id) as opposing_team, Players_has_Games.rebounds, Players_has_Games.blocks, Players_has_Games.steals, Players_has_Games.turnovers, Players_has_Games.minutes_played, Players_has_Games.started_game, Players_has_Games.freethrows_attempt, Players_has_Games.freethrows_made, Players_has_Games.field_goals_attempt, Players_has_Games.field_goals_made, Players_has_Games.3_points_attempt as three_points_attempt, Players_has_Games.3_points_made as three_points_made, Players_has_Games.assists, Players_has_Games.fouls, Players_has_Games.player_id, Players_has_Games.game_id FROM Players_has_Games JOIN Players ON Players_has_Games.player_id = Players.player_id JOIN Games ON Games.game_id=Players_has_Games.game_id JOIN Teams_has_Games ON Teams_has_Games.game_id=Games.game_id;"
 
     db.pool.query(query1, function (err, results, fields) {
         console.log(results, err)
@@ -141,6 +141,23 @@ app.put("/UpdatePlayer/:_id", function(req,res) {
         })
         })
 
+app.put("/UpdateStat/:_id", function(req,res) {
+    const ids = req.params._id.split("&");
+    const player_id = ids[0]
+    const game_id = ids[1]
+    console.log(req.body)
+    const query = `UPDATE Players_has_Games SET player_id='${player_id}', game_id='${game_id}', rebounds=${req.body.rebounds}, blocks=${req.body.blocks}, steals=${req.body.steals}, turnovers=${req.body.turnovers}, minutes_played=${req.body.minutes_played}, started_game=${req.body.started_game}, freethrows_attempt=${req.body.freethrows_attempt}, freethrows_made=${req.body.freethrows_made}, field_goals_attempt=${req.body.field_goals_attempt}, field_goals_made=${req.body.field_goals_made}, 3_points_attempt=${req.body.three_points_attempt}, 3_points_made=${req.body.three_points_made}, assists=${req.body.assists}, fouls=${req.body.fouls} WHERE player_id=${player_id} AND game_id=${game_id}`
+
+
+    db.pool.query(query, function (err, results, fields) {
+        // console.log(results, fields, err)
+        db.pool.query(query, function (err, results, fields) {
+            console.log(`error: ${err}`)
+            res.status(201).json(results);
+            })
+        })
+        })
+
 app.post("/addGame", function(req,res) {
     const query1 = `INSERT INTO Games (date) VALUES ('${req.body.date}');`
     const query2 = `INSERT INTO Teams_has_Games (home_team_id, away_team_id, game_id, home_team_score, away_team_score) VALUES (${req.body.homeTeam}, ${req.body.awayTeam}, (SELECT max(game_id) from Games), ${req.body.homeScore}, ${req.body.awayScore});`
@@ -196,11 +213,12 @@ app.post("/addPlayer", function(req,res) {
 })
 
 app.post("/addStat", function(req,res) {
-    const query1 = `INSERT INTO Players_has_Games (player_id,	game_id,	rebounds,	blocks,	steals,	turnovers,	minutes_played,	started_game,	freethrows_attempt,	freethrows_made,	field_goals_attempt,	field_goals_made,	3_points_attempt,	3_points_made,	assists,	fouls) VALUES '${req.body.player_id}', '${req.body.game_id}', '${req.body.rebounds}', '${req.body.blocks}', '${req.body.steals}', '${req.body.turnovers}', '${req.body.minutes_played}', '${req.body.started_game}', '${req.body.freethrows_attempt}', '${req.body.freethrows_made}', '${req.body.field_goals_attempt}', '${req.body.field_goals_made}', '${req.body.three_points_attempt}', '${req.body.three_points_made}', '${req.body.assists}', '${req.body.fouls}', ;`
+    const query1 = `INSERT INTO Players_has_Games (player_id,	game_id,	rebounds,	blocks,	steals,	turnovers,	minutes_played,	started_game,	freethrows_attempt,	freethrows_made,	field_goals_attempt,	field_goals_made,	3_points_attempt,	3_points_made,	assists,	fouls) VALUES ('${req.body.player_id}', '${req.body.game_id}', '${req.body.rebounds}', '${req.body.blocks}', '${req.body.steals}', '${req.body.turnovers}', '${req.body.minutes_played}', '${req.body.started_game}', '${req.body.freethrows_attempt}', '${req.body.freethrows_made}', '${req.body.field_goals_attempt}', '${req.body.field_goals_made}', '${req.body.three_points_attempt}', '${req.body.three_points_made}', '${req.body.assists}', '${req.body.fouls}') ;`
 
     db.pool.query(query1, function (err, results, fields) {
+        console.log(err)
         if (err != null) {
-            res.status(500).json({ Error: 'New player failed'})
+            res.status(500).json({ Error: 'New stat failed'})
             
         } else {
             res.status(201).json(results) }});
